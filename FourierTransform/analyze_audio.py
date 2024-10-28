0d575020d33b70c892df3e32862c32115489423b
Jordi Faraudo <jfaraudo@icmab.es>
2024-10-28 16:25:48 +0100

    Add files via upload

---
+++ FourierTransform/analyze_audio.py
@@ -0,0 +1,48 @@
+import matplotlib
+matplotlib.use("TkAgg")
+import numpy as np #importing Numpy with an alias np
+import pyaudio as pa 
+import struct 
+import matplotlib.pyplot as plt 
+
+CHUNK = 1024 * 1
+FORMAT = pa.paInt16
+CHANNELS = 1
+RATE = 44100 # in Hz
+RATE = 8000 # in Hz
+
+p = pa.PyAudio()
+
+stream = p.open(
+    format = FORMAT,
+    channels = CHANNELS,
+    rate = RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK
+)
+
+
+
+fig, (ax,ax1) = plt.subplots(2)
+x_fft = np.linspace(0, RATE, CHUNK)
+x = np.arange(0,2*CHUNK,2)
+line, = ax.plot(x, np.random.rand(CHUNK),'r')
+line_fft, = ax1.semilogx(x_fft, np.random.rand(CHUNK), 'b')
+ax.set_ylim(-32000,32000)
+ax.ser_xlim = (0,CHUNK)
+ax.set_xlabel('time frame')
+ax.set_ylabel('Intensity (a.u.)')
+ax1.set_xlim(20,RATE/2)
+ax1.set_ylim(0,1)
+ax1.set_xlabel('Freq. Hz')
+ax1.set_ylabel('Contribution')
+fig.show()
+
+while 1:
+    data = stream.read(CHUNK)
+    dataInt = struct.unpack(str(CHUNK) + 'h', data)
+    line.set_ydata(dataInt)
+    line_fft.set_ydata(np.abs(np.fft.fft(dataInt))*2/(11000*CHUNK))
+    fig.canvas.draw()
+    fig.canvas.flush_events()
